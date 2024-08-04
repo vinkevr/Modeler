aa83f636036f225db5d3d26112d2bed2bfb45a4d
Jorge Merlin <georgejimenez257@gmail.com>
2024-08-03 18:28:25 -0600

    [AD-02] feat: creation of the method to have a chat room

--- src/components/ui/Chat.jsx
+++ src/components/ui/Chat.jsx
@@ -1,37 +1,91 @@
-import React from 'react'
-import send from '../../images/send.png'
-import Message from './Message'
-import '../../site.css'
-import MessageIA from './MessageIA'
-import MessageSent from './MessageSent'
+import { useState, useEffect, useContext } from 'react';
+import { io } from 'socket.io-client';
+import UserContext from '../../context/UserContext';
+import send from '../../images/send.png';
+import Message from './Message';
+import '../../site.css';
+import MessageSent from './MessageSent';
 
-const Chat = () => {
+const socket = io('http://localhost:6679', {
+  transports: ['websocket'] // Forzar el uso de websocket
+});
 
-  
+const Chat = ({ idProject }) => {
+  const { user } = useContext(UserContext);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  useEffect(() => {
+    // Traer los mensajes si existen
+    const GetMessages = async () => {
+      const url = `http://localhost:6679/messages?room=${idProject}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setMessages(data);
+      console.log('Mensajes obtenidos de la bd');
+      console.log(data);
+    };
+    GetMessages();
+
+    // Añadir al usuario al chatRoom
+    socket.emit('joinRoom', idProject);
+
+    // Escuchar mensajes de la room
+    const messageListener = ({ message, idUser, userName }) => {
+      console.log(`Mensaje recibido: ${message}`);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { content: message, userId: idUser, username: userName }
+      ]);
+    };
+
+    socket.on('message', messageListener);
+
+    // limpiar el socket
+    return () => {
+      socket.off('message', messageListener);
+    };
+  }, [idProject]);
+
+  const handleMessage = async () => {
+    // Verificar que si se tenga un mensaje
+    if (newMessage === '') return;
+    console.log(`Mensaje enviado: ${newMessage}`);
+    // Enviar el mensaje al servidor
+    socket.emit('message', { message: newMessage.trim(), idUser: user.id, userName: user.nombre }, idProject);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { content: newMessage, userId: user.id, username: user.nombre }
+    ]);
+    setNewMessage('');
+  };
 
   return (
-    <>  
-      
+    <>
       <div className='bg-stone-200 h-2/3 overflow-auto'>
-       
-            <Message />
-            <Message />
-            <MessageSent />
-            <Message />
-            <Message />
-            <Message />
-        
+        {messages.map((message, i) => {
+          if (message.userId == user.id) {
+            return <MessageSent key={i} message={message} />;
+          } else {
+            return <Message key={i} message={message} />;
+          }
+        })}
       </div>
-
-        <div className='flex'>
-            <input className=' ml-5 w-4/5 h-5/6 shadow-lg border border-gray-300 px-6 py-3 rounded-lg focus:outline-none focus:outline-blue-500 focus:shadow-outline'></input>
-            <button className=''>
-                <img src= {send} className='h-8 w-8'/>
-            </button>
-        </div>
-
+      <div className='flex'>
+        <input
+          className='ml-5 w-4/5 h-5/6 shadow-lg border border-gray-300 px-6 py-3 rounded-lg focus:outline-none focus:outline-blue-500 focus:shadow-outline'
+          onChange={(e) => setNewMessage(e.target.value)}
+          value={newMessage}
+        />
+        <button
+          className=''
+          onClick={handleMessage}
+        >
+          <img src={send} className='h-8 w-8' />
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- src/components/ui/MessageSent.jsx
+++ src/components/ui/MessageSent.jsx
@@ -5,10 +5,10 @@ import messagefrom from '../../images/messagefrom.png';
 const MessageSent = ({ message }) => {
   return (
     <>
-      {message.sender === 'user' ? (
+      {message?.userId ? (
         <div className='flex ml-24 mt-5 justify-end'>
           <div className='bg-blue-300 w-auto px-5 py-2 rounded-l-lg rounded-b-lg shadow-lg flex flex-col'>
-            {message.message}
+            {message.content}
           </div>
           <img src={messageSent} className='h-5 w-5 -ml-1' alt="Message Sent Icon" />
         </div>
